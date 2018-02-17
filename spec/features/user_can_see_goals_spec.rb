762ab395d40b1c48a5e67f7111d682b0eaac33dc
Sal Espinosa <sespinos@gmail.com>
2018-02-17 03:12:46 -0700

    Update GoalsController to only pull goals for a current user

--- spec/features/user_can_see_goals_spec.rb
+++ spec/features/user_can_see_goals_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 describe "When I visit '/goals'" do
   it "I can see a collection of goals" do
-    goal1 = Goal.create(focus_area: 0, level: 0, description: "Speak at a conference")
-    goal2 = Goal.create(focus_area: 1, level: 1, description: "Speak at a big conference")
-    goal3 = Goal.create(focus_area: 3, level: 2, description: "Learn Haskell")
+    user = User.create(uid: "12", nickname: "s-espinosa", name: "Sal Espinosa", token: "12345")
+    user.goals.create(focus_area: 0, level: 0, description: "Speak at a conference")
+    user.goals.create(focus_area: 1, level: 1, description: "Speak at a big conference")
+    user.goals.create(focus_area: 3, level: 2, description: "Learn Haskell")
 
-    visit '/goals'
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_goals_path(user)
 
     expect(page).to have_content("Speak at a conference")
     expect(page).to have_content("Learn Haskell")
