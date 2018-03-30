ab0b3a4f3aee7711cc1de0accceb05cac84f8291
Sal Espinosa <sespinos@gmail.com>
2018-03-30 13:00:50 -0600

    Add test

---
+++ spec/features/user_can_add_a_goal_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'As a logged in user' do
+  describe 'when I visit my goals page' do
+    it 'I can add a goal' do
+      user = User.create(uid: "1234", nickname: "s-espinosa", name: "Sal Espinosa", token: "123456")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/goals'
+      click_on "Create New Goal"
+      fill_in "goal[description]", with: "Speak at a conference"
+      select "Expected", from: "goal[level]"
+      select "Community", from: "goal[focus_area]"
+    end
+  end
+end
